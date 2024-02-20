d43d099c7c66c54d929a53b9ae601fd85643a243
Aman Chourasia <76176138+DharmicDeveloper@users.noreply.github.com>
2024-02-21 02:02:17 +0530

    2nd day

---
+++ main.py
@@ -0,0 +1,10 @@
+# Python file to run code, which was present in the .txt files so that i can get a practical experience.
+# This is a test file, the code could be changed.
+
+length = float(input("Length: "))
+width = float(input("Width: "))
+height = float(input("Height: "))
+
+volume = length * width * height
+
+print(f"The volume is: {volume}cm^3")
